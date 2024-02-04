84d0f0e3bd3c01f53ffbe849e5b8a02346c86f68
Evrandil <grzes.zapala@gmail.com>
2024-02-04 12:50:50 +0100

    Ready maybe

---
+++ README.md
@@ -0,0 +1,9 @@
+sudo pacman -Syu
+sudo pacman -S --needed git github-cli
+cd ~
+mkdir gitTrash
+cd gitTrash
+gh auth login
+git clone https://github.com/Evrandil/Lapek --branch hyprland
+cd Lapek
+bash install.sh
--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+
 mv .config/* ~/.config
-mkdir ~/gitTrash
-mv FiraCode.zip ~/gitTrash
-cd ~/gitTrash
 p7 x FiraCode.zip -oFiraCode
 sudo mv FiraCode /usr/share/fonts
 sudo pacman -Syu
 sudo pacman -S hyprpaper vim neovim ranger neofetch easyeffects chroumium obsidian spotifyd powerline-fonts wireplumber discord p7zip
 sudo pacman -S --needed base-devel
+cd ~/gitTrash
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
@@ -22,4 +21,18 @@ sudo pacman -S signal-desktop telegram-desktop
 paru -S disneyplus
 paru -S messenger-nativefier
 paru -S whatsapp-for-linux
-
+cd ~/.config
+git clone https://github.com/Evrandil/nvim
+cd /usr/share
+sudo mkdir User
+sudo chown evrandil User
+cd User
+mkdir Memes
+mkdir Browser
+cd Browser
+git clone https://github.com/Evrandil/HomePage
+cd ~
+mkdir Gigachad
+cat gigachad.txt
+mv ~/gitTrash/Lapek/gigachad.txt Gigachad
+echo "Remember to log into Discord, signal, telegram, Disney+, messenger & whatsapp accounts"
