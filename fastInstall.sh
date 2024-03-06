5cbce4948019be265b3cb13d213912e7264cc947
Evrandil <grzes.zapala@gmail.com>
2024-03-06 23:18:37 +0100

    adsf

--- fastInstall.sh
+++ fastInstall.sh
@@ -4,10 +4,20 @@
 
 # Pacman {{{
 
-sudo pacman -S --needed --noconfirm --disable-download-timeout base-devel julia php go cairo curl calc p7zip sed tldr fd ripgrep fzf vim neovim neofetch ranger hyprland kitty hyprpaper obsidian powerline-fonts bluez signal-desktop telegram-desktop discord pipewire wireplumber spotifyd easyeffects chromium btop grim slurp calcurse testdisk gtk-layer-shell rustup neovide cmatrix nvtop bookworm gimp networkmanager bluez kalgebra nmap inetutils bat gzip ruby
+sudo pacman -S --needed --noconfirm --disable-download-timeout base-devel julia php go cairo curl calc p7zip sed tldr fd ripgrep fzf vim neovim neofetch ranger hyprland kitty hyprpaper obsidian powerline-fonts bluez signal-desktop telegram-desktop discord pipewire wireplumber spotifyd easyeffects chromium btop grim slurp calcurse testdisk gtk-layer-shell rustup neovide cmatrix nvtop bookworm gimp networkmanager bluez kalgebra nmap inetutils bat gzip ruby nautilus
 # sudo pacman -S openvpn qopenvpn
 # sudo pacman -S bc brainfuck
 rustup default stable
+
+
+
+# Pentesting tools {{{
+
+# sudo pacman -S termshark
+
+# }}}
+
+
 
 # }}}
 
@@ -57,6 +67,17 @@ mv .config/eww ~/.config
 # Paru {{{
 
 paru -S hyprshot tofi messenger-nativefier whatsapp-for-linux spotify-tui disneyplus netflix bluetuith nvim-packer-git
+
+
+
+# Pentesting tools {{{
+
+# paru -S ruby-evil-winrm gobuster dirbuster gobuster dirbuster
+# Wordlist !!!
+
+# }}}
+
+
 
 # }}}
 
