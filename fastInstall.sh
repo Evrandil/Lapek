e5c6bffb656d62330f6ef77331950c44fecf143d
Evrandil <grzes.zapala@gmail.com>
2024-03-27 22:05:29 +0000

    suckless terminal

--- fastInstall.sh
+++ fastInstall.sh
@@ -66,7 +66,7 @@ mv .config/eww ~/.config
 
 # Paru {{{
 
-paru -S hyprshot tofi messenger-nativefier whatsapp-for-linux spotify-tui disneyplus netflix bluetuith nvim-packer-git nerdfetch buddhasay biblesay termtrack term-sudoku
+paru -S hyprshot tofi messenger-nativefier whatsapp-for-linux spotify-tui disneyplus netflix bluetuith nvim-packer-git nerdfetch buddhasay biblesay termtrack term-sudoku st
 
 
 
